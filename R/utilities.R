b3ae64babf49a030b67c01318f9d4f6540415657
thartbm <thartbm@gmail.com>
2019-02-16 09:20:52 -0500

    added function for confidence intervals

---
+++ R/utilities.R
@@ -0,0 +1,29 @@
+getConfidenceInterval <- function(data, variance = var(data), conf.level = 0.95, method='t-distr', FUN=mean) {
+  
+  if (method %in% c('t-distr','t')) {
+    
+    z = qt((1 - conf.level)/2, df = length(data) - 1, lower.tail = FALSE)
+    
+    xbar = mean(data)
+    sdx = sqrt(variance/length(data))
+    
+    return(c(xbar - z * sdx, xbar + z * sdx))
+    
+  }
+  
+  if (method %in% c('bootstrap','b')) {
+    
+    data <- data[which(is.finite(data))] #need is.finite due to NA values
+    
+    samplematrix <- matrix(sample(data, size = resamples*length(data), replace = TRUE), nrow = resamples)
+    #apply mean function to this new matrix
+    BS <- apply(samplematrix, c(1), FUN=FUN) 
+    
+    lo <- (1-conf.level)/2.
+    hi <- 1 - lo
+    
+    return(quantile(BS, probs = c(lo,hi)))
+    
+  }
+  
+}
