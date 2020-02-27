40edb7bc51ecd7cfc3c2d65fde4e25b313f0a843
thartbm <thartbm@gmail.com>
2020-02-27 11:12:33 -0500

    moved rotateCoordinates() into reachProcessing.R and fixed a bug in getReachAngleAt()

--- R/reachProcessing.R
+++ R/reachProcessing.R
@@ -59,6 +59,38 @@ getSplinedVelocity <- function(x, y, t, spar=0.01) {
 }
 
 
+#' @title Rotate 2D trajectory
+#' @param df A dataframe or matrix with two columns: X and Y coordinates.
+#' @param angle An angle in degrees to rotate the trajectory by.
+#' @param origin A vector with the coordinates to rotate around. Default (0,0)
+#' @return Data frame with the rotated trajectory coordinates.
+#' @description Rotate a trajectory of X,Y coordinates.
+#' @details Not yet.
+#' @examples
+#' Not yet.
+#' @export
+rotateCoordinates <- function(df,angle,origin=c(0,0)) {
+  
+  df.names <- names(df)
+  
+  # create rotation matrix to rotate the X,Y coordinates
+  th <- (angle/180) * pi
+  R <- t(matrix(data=c(cos(th),sin(th),-sin(th),cos(th)),nrow=2,ncol=2))
+  
+  # put coordinates in a matrix, and subtract origin
+  coordinates <- sweep(as.matrix(df), 2, origin)
+  
+  # rotate the coordinates, add the origin back in
+  df <- as.data.frame(sweep(coordinates %*% R, 2, origin*-1))
+  
+  # restore column names
+  names(df) <- df.names
+  
+  # return the rotated coordinates
+  return(df)
+  
+}
+
 #' @title Angular deviation of a reach from target at a specific point. 
 #' @param trialdf Data frame representing the reach.
 #' @param location String specifying which location to use.
@@ -117,7 +149,7 @@ getReachAngleAt <- function(trialdf, location='pr0.33333', posunit='pix', timeun
   # rotate the trajectory:
   # - avoids problems with atan2 angles around 180 / -180
   # - puts the target at 0, so angular deviation is easy to get
-  trajectory <- SMCL::rotateCoordinates(x,y,-1*angle)
+  trajectory <- SMCL::rotateCoordinates(df(x,y),-1*angle)
   x <- trajectory[,1]
   y <- trajectory[,2]
   
